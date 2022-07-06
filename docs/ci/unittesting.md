5aead071da65436948ea4a82bc1a33f8d8d71fef
krande <kristoffer_andersen@outlook.com>
2022-07-06 13:13:56 +0200

    docs: add more info on unittesting. And added more files to the unittest itself

--- docs/ci/unittesting.md
+++ docs/ci/unittesting.md
@@ -5,4 +5,11 @@
 A github actions workflow [ci-edgedb.yml](../../.github/workflows/ci-edgedb.yml) has been created to test different
 IFC schema's and relevant inserts/queries based on specific IFC files. 
 
-Currently the github actions workflow fails on 2 of 3 IFC files
+At the time of writing the GitHub actions workflow fails on 2 of 3 IFC files on the "fingerprint assertion". The 
+"cube-advanced-brep.ifc" is the only IFC file that currently passes. 
+
+Here is the complete pytest setup for roundtrip testing of IFC files
+
+```{literalinclude} ../../tests/edge_model/server/test_ifc_file_roundtripping.py
+:language: python
+```
--- tests/edge_model/server/test_ifc_file_roundtripping.py
+++ tests/edge_model/server/test_ifc_file_roundtripping.py
@@ -7,7 +7,16 @@ from ifcdb import EdgeIO
 from ifcdb.edge_model.query_utils import validate_ifc_objects
 
 
-@pytest.mark.parametrize("ifc_file_name", ["tessellated-item.ifc", "cube-advanced-brep.ifc", "SpatialHierarchy1.ifc"])
+@pytest.mark.parametrize(
+    "ifc_file_name",
+    [
+        "tessellated-item.ifc",
+        "cube-advanced-brep.ifc",
+        "SpatialHierarchy1.ifc",
+        "MyBeam.ifc",
+        "MyBeamWithHoles.ifc",
+    ],
+)
 def test_roundtrip_ifc_files_validation(ifc_files_dir, ifc_file_name):
     db_name = ifc_file_name.replace(".ifc", "").replace("-", "_")
     ifc_file = ifc_files_dir / ifc_file_name
@@ -18,14 +27,11 @@ def test_roundtrip_ifc_files_validation(ifc_files_dir, ifc_file_name):
 
     with EdgeIO(db_schema_dir=db_schema_dir, database=db_name) as io:
         # Set up Schema & Database
-        client = io.create_client()
         io.create_schema(from_ifc_file=ifc_file)
         io.setup_database(delete_existing_migrations=True)
 
         # Insert IFC elements
-        for tx in client.transaction():
-            with tx:
-                io._insert_items_sequentially(ifc_file, tx)
+        io.insert_ifc(ifc_file)
 
         # Query & Validate Data using IfcOpenShell
-        validate_ifc_objects(io.ifc_io.ifc_obj, io.to_ifcopenshell_object(client=client))
+        validate_ifc_objects(io.ifc_io.ifc_obj, io.to_ifcopenshell_object())
