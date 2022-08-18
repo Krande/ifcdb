7aeb998bf864389645a8649d334a4f2394e73fc6
krande <kristoffer_andersen@outlook.com>
2022-08-18 13:49:20 +0200

    [wip] - update schema yml file

--- src/_dev/my_cube.py
+++ src/_dev/my_cube.py
@@ -4,11 +4,20 @@ from dataclasses import dataclass
 from pydantic import BaseModel
 
 
-class IfcGeneric(BaseModel):
+@dataclass
+class IfcGeneric:
     class_name: str
-    GlobalId: str
+    global_id: str
     props: dict
     links: dict[str, IfcGeneric]
+
+    def to_dict(self):
+        return dict(
+            class_name=self.class_name,
+            global_id=self.global_id,
+            props=self.props,
+            links={key: value.to_dict() for key, value in self.links.items()},
+        )
 
 
 def to_generic(ifc_elem: ifcopenshell.entity_instance) -> IfcGeneric:
@@ -21,14 +30,15 @@ def to_generic(ifc_elem: ifcopenshell.entity_instance) -> IfcGeneric:
     }
     props = {key: value for key, value in product_data.items() if not isinstance(value, ifcopenshell.entity_instance)}
 
-    return IfcGeneric(class_name, guid, props, links)
+    return IfcGeneric(class_name=class_name, global_id=guid, props=props, links=links)
 
 
 f = ifcopenshell.open("../../files/MyCube.ifc")
 product = f.by_type("IfcSlab")[0]
 assert isinstance(product, ifcopenshell.entity_instance)
+print(f.traverse(product))
 ifc_gen = to_generic(product)
-print(ifc_gen)
+print(ifc_gen.to_dict())
 
 # for key, value in product.get_info().items():
 #     print(key, value)
